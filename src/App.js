f419b1f4feae8dd06abd8dadf55303cb6098da3f
Jennifer Calhoon <jennifer.calhoon@interpreta.com>
2020-02-26 14:44:08 -0800

    updated Approval card using large prop

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import CommentDetail from './components/CommentDetail';
 import ApprovalCard from './components/ApprovalCard';
 
 const App = () => {
+  
   return (
     <div>
       <div className="ui container comments">
+        <ApprovalCard>
+          <CommentDetail author="Todd" time="Today at 1:54PM" comment="sweeeet" />
+        </ApprovalCard>
         <CommentDetail author="Sam" time="Today at 4:54PM" comment="Nice blog post!" />
         <CommentDetail author="Jen" time="Today at 3:54PM" comment="this is so cool!" />
-        <CommentDetail author="Todd" time="Today at 1:54PM" comment="sweeeet" />
-        <ApprovalCard />
       </div>
     </div>
   );
